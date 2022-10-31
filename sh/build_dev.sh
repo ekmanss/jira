1739196a8f9e8d70d59e7e1c5775a437e565d596
Roy <hfers@msn.com>
2022-10-31 17:46:48 +0800

    update

---
+++ sh/build_dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# M1 instruction sets
+#docker build -t  37976120/jira --platform=linux/arm64 .
+
+#inter or amd instruction sets
+#docker build -t  37976120/jira --platform=linux/amd64 .
+
+docker build -t  37976120/jira .
